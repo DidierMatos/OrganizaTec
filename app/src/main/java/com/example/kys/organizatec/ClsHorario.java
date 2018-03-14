fda72f098e759f525c9eb311f7638fe7c28667db
DidierMatos <didiermat00s@gmail.com>
2018-03-14 03:22:24 -0500

    Se agrego su layout de ClsHorario para poder redirigir mas tarde al validar los spinner

--- app/src/main/java/com/example/kys/organizatec/ClsHorario.java
+++ app/src/main/java/com/example/kys/organizatec/ClsHorario.java
@@ -1,8 +1,21 @@
 package com.example.kys.organizatec;
 
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
 /**
  * Created by Didier on 14/03/2018.
  */
 
-public class ClsHorario {
+public class ClsHorario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lytclshorario);
+    }
+
 }
